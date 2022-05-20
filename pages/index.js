2a1948bfae73469293319ecc1f39654afdd0e5d6
kiarashmajdi <kiamaj.ca@gmail.com>
2022-05-20 10:03:53 -0400

    title updated

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 import ContactMe from '../components/ContactMe';
+
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
